393158dc7eed21c50eae2ad44473bf40453f5d27
Mohan <notcoldum@gmail.com>
2021-04-26 06:51:19 +0800

    expo image picker - select from gallery & camera

--- src/components/CameraScreen.js
+++ src/components/CameraScreen.js
@@ -1,12 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
-    Button, Text, View, StyleSheet
+    Button, Text, View, StyleSheet, Platform, Image
 } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
 const CameraScreen = (props) => {
+    // String: contains image URL
+    const [image, setImage] = useState('');
+
+    const takeImage = async () => {
+        // FIXME: requests unhandled after initial rejection
+        if (Platform.OS !== 'web') {
+            const { status } = await ImagePicker.requestCameraPermissionsAsync();
+            if (status !== 'granted') {
+                alert('Sorry, we need camera roll permissions to make this work!');
+                return;
+            }
+        }
+
+        let result = await ImagePicker.launchCameraAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: true,
+            aspect: [1, 1],
+            quality: 1,
+        });
+
+        console.log(result);
+
+        if (!result.cancelled) {
+            setImage(result.uri);
+        }
+    };
+
+    const selectImage = async () => {
+        // need testing
+        if (Platform.OS !== 'web') {
+            const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+            if (status !== 'granted') {
+                alert('Sorry, we need camera roll permissions to make this work!');
+                return;
+            }
+        }
+
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: true,
+            aspect: [1, 1],
+            quality: 1,
+        });
+
+        console.log(result);
+
+        if (!result.cancelled) {
+            setImage(result.uri);
+        }
+    };
+
+    const displayPreview = () => {
+        if (image === '')
+            return <Text style={styles.ImagePreview}>Upload Image to Start</Text>
+        else
+            return <Image source={{ uri: image }} style={styles.ImagePreview} />
+    }
+
     return (
         <View style={styles.CameraScreen}>
             <Text>Camera Screen</Text>
+            {displayPreview()}
+            <Button title="Choose Photo from Library" onPress={selectImage} />
+            <Button title="Take Photo using Camera" onPress={takeImage} />
             <Button title="Back to Home Screen" onPress={() => props.history.push('/')} />
         </View>
     );
@@ -19,7 +81,22 @@ const styles = StyleSheet.create({
         backgroundColor: 'honeydew',
         width: '100%',
         height: '80%'
+    },
+    ImagePreview: {
+        width: 250,
+        height: 250
     }
 });
 
 export default CameraScreen;
+
+// useEffect(() => {
+    //     (async () => {
+    //         if (Platform.OS !== 'web') {
+    //             const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    //             if (status !== 'granted') {
+    //                 alert('Sorry, we need camera roll permissions to make this work!');
+    //             }
+    //         }
+    //     })();
+    // }, []);
